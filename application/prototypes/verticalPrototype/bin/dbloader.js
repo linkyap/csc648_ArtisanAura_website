4eea85628e1dd477af2b233e784160bc7dc00102
James Donnelly <jrdonnelly468@gmail.com>
2023-07-09 21:43:11 -0700

    yee

--- application/prototypes/verticalPrototype/bin/dbloader.js
+++ application/prototypes/verticalPrototype/bin/dbloader.js
@@ -6,7 +6,7 @@ const connectionConfig = {
   user: 'root',
   password: 'jnrscZTyvbdC1YG/',
   database: 'artisanAura',
-  privateKey: './credentials/id_rsa',//maybe add ../ infrom if doesnt work basd
+  privateKey: 'credentials/id_rsa',//maybe add ../ infrom if doesnt work basd
 };
 
 async function main() {
