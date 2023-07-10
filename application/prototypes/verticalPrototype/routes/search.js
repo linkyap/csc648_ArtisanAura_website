c43e13c3c658adad66091e85fc243131e03ad7db
eddaww <cai01edwin@gmail.com>
2023-07-10 14:52:11 -0700

    updated search.js (searching description, type and material now) added 6 image for testing the search function; edited header.hbs get search; added css when displaying resutlt

--- application/prototypes/verticalPrototype/routes/search.js
+++ application/prototypes/verticalPrototype/routes/search.js
@@ -6,16 +6,21 @@ router.get('/search', async (req, res) => {
   const searchTerm = req.query.q; // Get the search query from the request
 
   try {
-    const [results, fields] = await db.execute(
-      `SELECT * FROM product WHERE name LIKE '%${searchTerm}%'`
-    );
+    if (searchTerm) {
+      const [results, fields] = await db.execute(
+        `SELECT * FROM product WHERE name LIKE '%${searchTerm}%' OR type LIKE '%${searchTerm}%' OR material LIKE '%${searchTerm}%' OR description LIKE '%${searchTerm}%'`
+      );
 
-    if (results.length > 0) {
-      // If items are found, render the searchresults template with the results
-      res.render('searchresults', { results });
+      if (results.length > 0) {
+        // If items are found, render the searchresults template with the results
+        res.render('searchresults', { results });
+      } else {
+        // If no items are found, render the searchresults template with a "Item not found!" message
+        res.render('searchresults', { notFound: true });
+      }
     } else {
-      // If no items are found, render the searchresults template with a "Item not found!" message
-      res.render('searchresults', { notFound: true });
+      // If the search term is empty, render the searchresults template with an empty results array
+      res.render('searchresults', { results: [] });
     }
   } catch (err) {
     console.error('Error executing search query:', err);
@@ -24,3 +29,4 @@ router.get('/search', async (req, res) => {
 });
 
 module.exports = router;
+
