551d10ef277e0730afe37a8a8ab517d308995f91
James Donnelly <jrdonnelly468@gmail.com>
2023-07-10 17:56:27 -0700

    MOVE OVER

--- application/prototypes/verticalPrototype/conf/database.js
+++ application/prototypes/verticalPrototype/conf/database.js
@@ -1,15 +1,15 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-  host: 'l34.72.218.74',
+  socketPath: '/cloudsql/csc-648-848-team-05:us-central1:artisan-aura-mysql-instance',
   user: 'root',
   database: 'artisanAura',
   password: 'jnrscZTyvbdC1YG/',
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  maxIdle: 10,
+  idleTimeout: 60000,
+  queueLimit: 0,
 }).promise();
-
-
 
 module.exports = pool;
