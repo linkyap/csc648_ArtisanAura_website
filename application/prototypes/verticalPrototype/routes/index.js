38ddb21ddd8f880c34815eca7ea6b98985557a19
denean le <dle@sfsu.edu>
2023-07-11 13:37:00 -0700

    removed conditional statement, kept original state of app

--- application/prototypes/verticalPrototype/routes/index.js
+++ application/prototypes/verticalPrototype/routes/index.js
@@ -1,21 +1,11 @@
 var express = require('express');
 const path = require("path");
 var router = express.Router();
-const db = require('../conf/database');
-
 
 // home page aka index
 router.get('/', async function (req, res, next) {
   try {       //vvv
-    let limitSql = await db.execute(`SELECT COUNT(*) FROM product`);
-    console.log("limit sql = ", limitSql);
-    if (limitSql >= 5) {
-        res.render('index', {title:'Home', css:["newsletter.css","quiz.css"], js:["quiz.js"], limitReached:true});
-    }
-    else{
-       res.render('index', { title:'Home', css:["newsletter.css","quiz.css"], js:["quiz.js"], limitReached:false});
-    }
-   
+    res.render('index', { title:'Home', css:["newsletter.css","quiz.css"], js:["quiz.js"]});
   } catch (err) {
     console.error(err);
     res.status(500).send("Server error");
