ff095bcb2f494b0cab0fadf7a1733384f0aaa61a
creddig1 <136027557+creddig1@users.noreply.github.com>
2023-07-23 14:46:03 -0700

    single product route back to coming soon

--- application/prototypes/horizontalPrototype/routes/index.js
+++ application/prototypes/horizontalPrototype/routes/index.js
@@ -34,7 +34,7 @@ router.get('/addProduct',function(req,res,next){
 
 // single product page 
 router.get('/product/:id', function(req,res,next){
-  res.render('product/:id');
+  res.render('ComingSoon');
 });
 
 // privacy policy page 
