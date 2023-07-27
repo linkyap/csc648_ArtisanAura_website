16a749e156d568607d070cf8d28a3c07b16b7cd8
creddig1 <136027557+creddig1@users.noreply.github.com>
2023-07-26 22:26:00 -0700

    copy horizontal files to beta prototype

---
+++ application/prototypes/betaPrototype/routes/index.js
@@ -0,0 +1,162 @@
+var express = require('express');
+const path = require("path");
+var router = express.Router();
+var db = require('../conf/database');
+// home page aka index
+router.get('/', async function (req, res, next) {
+  try {       //vvv
+    res.render('index', { title:'Home', css:["newsletter.css","quiz.css"], js:["quiz.js"]});
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server error");
+  }
+});
+
+// login page 
+router.get('/login',function(req,res,next){
+  res.render('login');
+});
+
+//profile page
+router.get('/users/profile/:id', function(req, res, next){
+  res.render('profile', {title:'Profile'});
+});
+
+// registration page 
+router.get("/registration", function(req,res,next){
+  res.render('registration');
+});
+
+// coming soon page 
+router.get('/ComingSoon',function(req,res,next){
+  res.render('ComingSoon', { title:'Coming Soon', css:["newsletter.css"]});
+});
+
+// add product page 
+router.get('/addProduct', function(req,res,next){
+  res.render('addProduct');
+});
+
+// Refund page
+router.get('/req-refund',function(req,res,next){
+  res.render('refundReq', { title:'Request Refund'});
+});
+// Order page
+router.get('/order-status',function(req,res,next){
+  res.render('orderStatus', { title:'Order Status'});
+});
+// Shopping cart page
+router.get('/cart', function (req,res,next){
+  res.render('cart', {title: 'Shopping Cart'});
+});
+
+// privacy policy page 
+router.get('/PrivacyPolicy',function(req,res,next){
+  res.render('PrivacyPolicy', { title:'ArtisanAura Privacy Policy'});
+});
+ // single product page 
+router.get('/productsingle', function(req,res,next){
+  res.render('productsingle');
+ });
+// custom product page 
+router.get('/customproduct', function(req,res,next){
+  res.render('customproduct');
+ });
+// terms and conditions page
+router.get('/termsConditions',function(req,res,next){
+  res.render('termsConditions', { title:'ArtisanAura Terms and Conditions'});
+});
+
+// terms of service and privacy for user registration page
+router.get('/termsService',function(req,res,next){
+  res.render('termsService', { title:'Terms of Service and Privacy Policy'});
+});
+
+// about us page
+router.get('/AboutUs', async function(req,res,next){
+  try {
+    var [products, fields] = await db.execute(
+      `SELECT * FROM product ORDER BY id DESC;`
+    );
+    if (products.length === 0) {
+      req.flash("error", `No products available`);
+    }
+    res.render('AboutUs', { title:'About ArtisanAura Jewelry',products: products, css:["newsletter.css","quiz.css","productspage.css"], js:["quiz.js"]});
+  }catch (err) {
+  console.error(err);
+  res.status(500).send("Server error");
+ }
+});
+
+
+// shop page
+router.get('/Shop', async function(req,res,next){
+    try {
+      let filterPrice = req.query.price; // Get the filter price from the query parameters
+
+      let query = "SELECT * FROM product";
+      let queryParams = [];
+  
+      // If a filter price is provided, add a WHERE clause to the SQL query
+      if (filterPrice) {
+        query += " WHERE price <= ?";
+        queryParams.push(parseFloat(filterPrice));
+      }
+  
+      const [products, fields] = await db.execute(query, queryParams);
+
+      // var [products, fields] = await db.execute(
+      //   `SELECT * FROM product ORDER BY id DESC;`
+      // );
+      if (products.length === 0) {
+        req.flash("error", `No products available`);
+      }
+      res.render('Shop', { title:'Shop ArtisanAura Jewelry',products: products, css:["newsletter.css","quiz.css","productspage.css"], js:["quiz.js"]});
+    }catch (err) {
+    console.error(err);
+    res.status(500).send("Server error");
+   }
+});
+
+
+// Guides page
+router.get('/Guides',function(req,res,next){
+  res.render('Guides', { title:'Guides', css:["newsletter.css","quiz.css"], js:["quiz.js"]});
+});
+// Refund page
+router.get('/Refund',function(req,res,next){
+  res.render('Refund', { title:'Refund', css:["newsletter.css","quiz.css"], js:["quiz.js"]});
+});
+
+// Customer Service page
+router.get('/CustomerSupport',function(req,res,next){
+  res.render('CustomerSupport', { title:'Customer Support', css:["newsletter.css","quiz.css"], js:["quiz.js"]});
+});
+
+// Newsletter sign up
+router.post('/Newsletter', async function(req,res,next){
+  var newsletter_Id = req.params.id;
+  var email = req.body.email;
+  console.log(`newsletter_Id: ${newsletter_Id}, email: ${email}`);
+
+  try {
+    var [input, _] = await db.execute(
+      "INSERT INTO newsletter (email) VALUES (?)",
+      [email]
+    );
+    if (input && input.affectedRows) {
+      req.flash("success", "Youve been added to the mailing list");
+      return req.session.save(function (error) {
+        if (error) next(error);
+        return res.redirect(`/`);
+      });
+    } else {
+      next(new Error("error occurred"));
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+
+module.exports = router;
---
+++ application/prototypes/betaPrototype/routes/users.js
@@ -0,0 +1,122 @@
+var express = require('express');
+var router = express.Router();
+var bcrypt = require('bcrypt');
+var db = require('../conf/database');
+const User = require('../db/users');
+const UserError = require('../helpers/userError');
+const registerValidator = require('../helpers/regValidation');
+
+router.use('/registration', registerValidator);
+router.post('/registration', (req, res, next) => {
+  let { name, email, password } = req.body;
+  User.emailExists(email)
+    .then((emailDoesExist) => {
+      if (emailDoesExist) {
+        throw new UserError(
+          "Email already exists",
+          "/registration",
+          200
+        );
+      }
+      else {
+        return User.create(name, email, password);
+      }
+    })
+    .then((createUser) => {
+      if (createUser < 0) {
+        throw new UserError(
+          "User could not be created",
+          "/registration",
+          500
+        );
+      }
+      else {
+        req.flash('success', 'Account has been made!');
+        req.session.save((err) => {
+          res.redirect('/login');
+        });
+      }
+    })
+    .catch((err) => {
+      if (err instanceof UserError) {
+        req.flash('error', err.getMessage());
+        res.status(err.getStatus());
+        req.session.save((err) => {
+          res.redirect('/registration');
+        });
+      }
+      else {
+        next(err);
+      }
+    })
+});
+
+router.post('/login', async function (req, res, next) {
+  const { email, password } = req.body;
+  User.authenticate(email, password)
+  .then((loggedUser) => {
+    if(loggedUser.id > 0){
+      req.session.account = {
+        id: loggedUser.id,
+        email: email,
+        name: loggedUser.name
+      };
+      req.flash('success', 'You are now logged in!');
+      req.session.save(err => {
+        res.redirect('/');
+      });
+    }
+    else{
+      throw new UserError(
+        "Invalid email and/or password", 
+        "/login",
+        200);
+    }
+  })
+  .catch((err) => {
+    if(err instanceof UserError) {
+      req.flash('error', err.getMessage());
+      res.status(err.getStatus());
+      req.session.save(err => {
+        res.redirect('/login');
+      })
+    }
+    else{
+      next(err);
+    }
+  })
+});
+
+router.post('/logout', function (req, res, next) {
+  req.flash('success', 'You are now logged out');
+  req.session.destroy(function (err) {
+    if (err) {
+      next(error);
+    }
+    return res.redirect('/');
+  })
+});
+
+router.post('/req-refund', (req, res, next) => {
+  req.flash('success', 'Refund confirmation sent to email');
+  req.session.save(err => {
+    res.redirect('/');
+  })
+});
+
+router.post('/order-status', (req, res, next) => {
+  req.flash('success', 'Order pending. Check again in 24 hours.');
+  req.session.save(err => {
+    res.redirect('/order-status');
+  })
+});
+
+router.post('/cart', (req, res, next) => {
+  req.flash('error', 'Shopping cart is empty');
+  req.session.save(err => {
+    res.redirect('/cart');
+  })
+});
+
+
+module.exports = router;
