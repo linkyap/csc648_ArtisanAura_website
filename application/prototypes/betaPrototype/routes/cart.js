782a9b413b8fd9e923a37844a7af2dc3be2bdc33
denean le <dle@sfsu.edu>
2023-08-04 22:13:28 -0700

    made changes to css for remove button

--- application/prototypes/betaPrototype/routes/cart.js
+++ application/prototypes/betaPrototype/routes/cart.js
@@ -16,10 +16,6 @@ router.get('/cart-list', async (req, res, next) => {
         const quantityList = await Promise.all(results.map(result => result.quantity));
         // Get product details 
         const cartList = await Promise.all(results.map(result => getDetails(result))); 
-        console.log("cartlist length : " + cartList.length);
-        console.log("quantitylist length : " + quantityList.length);
-        console.log("quantity[0] : " + quantityList[0]);
-        console.log("quantity[0][0] : " + quantityList[0][0]);
         if (cartList.length > 0) {
             let subtotal = 0;
             cartList.forEach(item => {
