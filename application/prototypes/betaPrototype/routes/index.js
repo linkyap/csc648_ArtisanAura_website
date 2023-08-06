1bea19c3c95dc205f9da5eb8f3a74d21d1a9b8f3
James Donnelly <jrdonnelly468@gmail.com>
2023-08-05 17:53:39 -0700

    reverting to older version for time being so you guys can still work, just for the shop and index shop route

--- application/prototypes/betaPrototype/routes/index.js
+++ application/prototypes/betaPrototype/routes/index.js
@@ -96,44 +96,27 @@ router.get('/AboutUs', async function(req,res,next){
 // shop page
 router.get('/Shop', async function(req, res, next){
   try {
-       // chk if rest clicked
-       if (req.query.reset === 'true') {
-        req.session.filters = {};
-        return res.redirect('/Shop');
-    }
-
       //for filtering
+      let filterPrice = req.query.price;
+      let filterType = req.query.type;
+      let filterMaterial = req.query.material;
+      let filterGemstone = req.query.gemstone;
+      let sortPrice = req.query.sort_price;
+      let min_price = req.query.min_price ? parseFloat(req.query.min_price) : null;
+      let max_price = req.query.max_price ? parseFloat(req.query.max_price) : null;
 
-     //session filter or empty
-     req.session.filters = req.session.filters || {};
-        
-     // filters in query
-     if (req.query.type) req.session.filters.type = req.query.type;
-     if (req.query.material) req.session.filters.material = req.query.material;
-     if (req.query.gemstone) req.session.filters.gemstone = req.query.gemstone;
-     if (req.query.sort_price) req.session.filters.sort_price = req.query.sort_price;
-     if (req.query.min_price) req.session.filters.min_price = parseFloat(req.query.min_price);
-     if (req.query.max_price) req.session.filters.max_price = parseFloat(req.query.max_price);
-
-     // filter from session
-     let {
-         type: filterType,
-         material: filterMaterial,
-         gemstone: filterGemstone,
-         sort_price: sortPrice,
-         min_price: min_price,
-         max_price: max_price
-     } = req.session.filters;
-
-
-     
         // all product data
       let query = "SELECT * FROM product";
       let queryParams = [];
-      let conditions = [];
+            // conditions for each filter
+      if (filterPrice || filterType || filterMaterial || filterGemstone) {
+          query += " WHERE ";
+          let conditions = [];
 
-
-          //each filter type below
+          if(filterPrice) {
+              conditions.push("price <= ?");
+              queryParams.push(parseFloat(filterPrice));
+          }
           if(filterType) {
               conditions.push("type = ?");
               queryParams.push(filterType);
@@ -159,10 +142,8 @@ router.get('/Shop', async function(req, res, next){
         }
 
             //joins multiple conditions
-          if (conditions.length > 0) {
-          query += " WHERE " + conditions.join(' AND ');
-        }
-      
+          query += conditions.join(' AND ');
+      }
         //edge, if price sort by it
       if(sortPrice) {
           query += ` ORDER BY price ${sortPrice}`;
@@ -174,7 +155,7 @@ router.get('/Shop', async function(req, res, next){
         return res.render('Shop', { error: 'No products available', products: [] });
       }//self explanatory ^^^
   
-      return res.render('Shop', { products: products, filters: req.session.filters });
+      return res.render('Shop', { products: products });
 
   } catch (err) {
       console.error(err);
