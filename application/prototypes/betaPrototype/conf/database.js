5d7bfa9f811294f2100d17b7a015eeed34831655
James Donnelly <jrdonnelly468@gmail.com>
2023-08-05 19:05:48 -0700

    thought it was not longer needed i think i was wrong

--- application/prototypes/betaPrototype/conf/database.js
+++ application/prototypes/betaPrototype/conf/database.js
@@ -11,3 +11,19 @@
 // }).promise();
 
 // module.exports = pool;
+
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+  socketPath: '/cloudsql/csc-648-848-team-05:us-central1:artisan-aura-mysql-instance',
+  user: 'root',
+  database: 'artisanAura',
+  password: 'jnrscZTyvbdC1YG/',
+  waitForConnections: true,
+  connectionLimit: 10,
+  maxIdle: 100,
+  idleTimeout: 60000,
+  queueLimit: 0,
+}).promise();
+
+module.exports = pool;
